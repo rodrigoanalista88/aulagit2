5ad091979e6be85c4a171bce7e865f7fcde1045c
Marllon Ramos <marllonramos@hotmail.com>
2017-10-09 20:56:06 -0300

    Correcao da validacao com js

--- validationJs.js
+++ validationJs.js
@@ -1,9 +1,11 @@
 document.getElementById('btnCadastrar').addEventListener('click', function(){
-	var nome = document.getElementById('nome').text;
-	var email = document.getElementById('email').text;
+	var nome = document.getElementById('nome').value;
+	var email = document.getElementById('email').value;
 
-	if(nome == undefined && email == undefined){
+	if(!nome && !email){
 		alert('Informe os dados!!!');
+	}else{
+		alert('Dados enviados!');
 	}
 
 });
